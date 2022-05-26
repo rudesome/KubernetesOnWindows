38d354711cf9b14e614aa68e6503d9b313c3efe9
Parkhost <info@parkhost.eu>
2022-05-26 21:17:41 +0200

    duplicate and add rm locations

--- windows/clean.ps1
+++ windows/clean.ps1
@@ -1,14 +1,17 @@
 $ErrorActionPreference="SilentlyContinue"
-Stop-Process -Name nssm -Force -confirm:$false -erroraction silentlycontinue
+Stop-Process -Name nssm -Force -confirm:$false
 if (Get-Service -Name Kubelet){
 	cmd.exe /c sc delete kubelet
 }
-Stop-Process -Name wins -Force -confirm:$false -erroraction silentlycontinue
+Stop-Process -Name wins -Force -confirm:$false
 Get-Hnsnetwork | Remove-Hnsnetwork
 Get-HnsNamespace | remove-HnsNamespace
-Get-NetFirewallRule -Name Kubelet -ea silentlycontinue | Remove-NetFirewallRule -ea silentlycontinue
+Get-NetFirewallRule -Name Kubelet -ea silentlycontinue | Remove-NetFirewallRule
 
 rm -force -r /k/* -confirm:$false
 rm -force -r /var -confirm:$false
 rm -force -r /etc -confirm:$false
 rm -force -r /opt -confirm:$false
+rm -force -r /sourcevip -confirm:$false
+rm -force -r /C -confirm:$false
+rm -force -r /etc -confirm:$false
