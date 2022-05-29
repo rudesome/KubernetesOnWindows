7b1131570bab81779a71976a0d75eb7ba9818163
Parkhost <info@parkhost.eu>
2022-05-29 16:34:24 +0200

    fix windows kubelet

--- windows/clean.ps1
+++ windows/clean.ps1
@@ -3,11 +3,11 @@ Stop-Process -Name nssm -Force -confirm:$false
 if (Get-Service -Name Kubelet){
 	cmd.exe /c sc delete kubelet
 }
-Stop-Process -Name wins -Force -confirm:$false
 Get-Hnsnetwork | Remove-Hnsnetwork
-Get-HnsNamespace | remove-HnsNamespace
-Get-NetFirewallRule -Name Kubelet -ea silentlycontinue | Remove-NetFirewallRule
+Get-HnsNamespace | Remove-HnsNamespace
+Get-NetFirewallRule -Name Kubelet | Remove-NetFirewallRule
 
+Stop-Process -Name wins -Force -confirm:$false
 rm -force -r /k/* -confirm:$false
 rm -force -r /var -confirm:$false
 rm -force -r /etc -confirm:$false
